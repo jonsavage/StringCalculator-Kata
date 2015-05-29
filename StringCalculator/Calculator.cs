68cbf9850adc06148de5b7267468fb7d03ed08e8
jsavage06 <jon5@vt.edu>
2015-05-29 17:24:27 -0400

    step4

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -1,30 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StringCalculator
 {
     public class Calculator
     {
+        private char[] defaultDelimiter = new char[] {','};
+
         public int Add(string s)
         {
-            if (s == "")
-                return 0;
+            char[] delimiters;
 
-            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '\n' };
+            if (CustomDelimiterSet(s))
+            {
+                delimiters = GetCustomDelimiter(s).Concat(defaultDelimiter).ToArray();
+            }
+            else
+            {
+                delimiters = defaultDelimiter;
+            }
+
+
+
+
+            //char[] delimiters = GetDelimiters(s);
+
+            string a = RemoveDelimiterInfo(s);
+            String[] input = RemoveDelimiterInfo(s).Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+
+            CheckForNegativeNumbers(input);
+
 
             int sum = 0;
-            String[] input = s.Split(delimiterChars);
-            
-
             for (int i = 0; i < input.Length; i++)
-            {
-                sum += Int32.Parse(input[i]);
+            {       
+                sum += int.Parse(input[i]);
             }
 
             return sum;
         }
+
+        public char[] GetCustomDelimiter(string input)
+        {
+            
+            {
+                return new char[] {input.Substring(2, input.IndexOf('\n')).ToCharArray()[0], '\n', ','};
+            }
+
+            return new char[] {' ', ',', '\n'};
+        }
+
+        public string RemoveDelimiterInfo(string input)
+        {
+            if (input.IndexOf("//") == 0)
+            {
+                return input.Substring(input.LastIndexOf("\n"));
+            }
+            return input;
+        }
+
+        public static void CheckForNegativeNumbers(IEnumerable<int> numbers)
+        {
+            if (numbers.Any(number => number < 0))
+            {
+                throw new ArgumentException("neg");
+            }
+        }
+
+        public bool CustomDelimiterSet(string input)
+        {
+            return Regex.IsMatch(input, @"//.{1,2}\n");
+        }
+        
     }
 }
--- StringCalculatorTests/UnitTests.cs
+++ StringCalculatorTests/UnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -16,11 +17,17 @@ namespace StringCalculatorTests
 
 
         [Test]
-        [TestCase("", Result = 0)]
+        [TestCase("", Result = 0)]                // Step 1
         [TestCase("1", Result = 1)]
         [TestCase("1,1", Result = 2)]
-        [TestCase("1,1", Result = 2)]
-        [TestCase("1\n2,3" , Result = 6)]
+        [TestCase("1,2", Result = 3)]
+        [TestCase("1,1,1,1,1,1,1,1", Result = 8)] // Step 2
+        [TestCase("1\n2,3", Result = 6)]          // Step 3
+        [TestCase("1\n2,3,4,5", Result = 15)]                    
+        [TestCase("//;\n1;2", Result = 3)]         // Step 4
+        [TestCase("//1\n213", Result = 5)]
+        [TestCase("//\t\n2\t3", Result = 5)]
+        [TestCase("//\t\n2,3\n5", Result = 5)]
         public int TestEmpty(string s)
         {
             var x = calculator.Add(s);
@@ -28,5 +35,20 @@ namespace StringCalculatorTests
             return x;
         }
 
+        [Test]
+        [TestCase("-1", "-1")]
+        [TestCase("-1,-2", "-1,-2")]
+        [TestCase("-1,1,-1", "-1,-1,-1")]
+        public void TestNegatives(string s, string expectedMessage)
+        {
+            Assert.Throws<ArgumentException>(
+                () => calculator.Add(s)
+            );
+
+            var exception = Assert.Throws<ArgumentException>(() => calculator.Add(s));
+
+            Assert.AreEqual(expectedMessage, exception.Message);
+
+        }
     }
 }
