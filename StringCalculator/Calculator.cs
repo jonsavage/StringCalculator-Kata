fa4dffeef77b6de730d02b10ed6e94755a2cfd75
jsavage06 <jon5@vt.edu>
2015-05-28 21:03:30 -0400

    initial

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string s)
+        {
+            if (s == "")
+                return 0;
+
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '\n' };
+
+            int sum = 0;
+            String[] input = s.Split(delimiterChars);
+            
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += Int32.Parse(input[i]);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ StringCalculatorTests/UnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StringCalculator;
+
+namespace StringCalculatorTests
+{
+   
+    public class UnitTests
+    {
+
+        Calculator calculator = new Calculator();
+
+
+        [Test]
+        [TestCase("", Result = 0)]
+        [TestCase("1", Result = 1)]
+        [TestCase("1,1", Result = 2)]
+        [TestCase("1,1", Result = 2)]
+        [TestCase("1\n2,3" , Result = 6)]
+        public int TestEmpty(string s)
+        {
+            var x = calculator.Add(s);
+
+            return x;
+        }
+
+    }
+}
